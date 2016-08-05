f6cbecc79e561eb9d27892cfcde65afc5e0e769a
Lukas -skywall- Sztefek <lukas.sztefek@thefuntasty.com>
2016-08-05 17:32:26 +0200

    add retention policy annotation

--- tfileautouploader/src/main/java/com/thefuntasty/tfileautouploader/Status.java
+++ tfileautouploader/src/main/java/com/thefuntasty/tfileautouploader/Status.java
@@ -2,12 +2,17 @@ package com.thefuntasty.tfileautouploader;
 
 import android.support.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class Status {
 	public static final int IDLE = 0;
 	public static final int UPLOADING = 1;
 	public static final int UPLOADED = 2;
 	public static final int FAILED = 3;
 	public static final int REMOVED = 4;
+
+	@Retention(RetentionPolicy.SOURCE)
 	@IntDef({IDLE, UPLOADING, UPLOADED, FAILED, REMOVED})
 	public @interface UploadStatus {}
 
